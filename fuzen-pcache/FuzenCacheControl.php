32fc0192ac0d5a26dd6934fad648bb837eccc00c
Frederico Araujo Soares <frederico.a.soares@gmail.com>
2013-03-28 00:12:33 -0300

    README.md created with how-to. Php object to manage cache files done and tested.

--- fuzen-pcache/FuzenCacheControl.php
+++ fuzen-pcache/FuzenCacheControl.php
@@ -9,18 +9,34 @@
  */
 class FuzenCacheControl {
 	
+	private $cachefile;
+	
 	/**
+	 * @param numeric $timeCache
+	 * @param String $pathFile
 	 * Method construct.
 	 */
-	function __construct() {
+	function __construct($timeCache, $pathFile) {
+		$this->cachefile = $pathFile;
+		$cachetime = $timeCache * 60; // minutes
 		
+		if (file_exists($this->cachefile) && (time() - $cachetime < filemtime($this->cachefile))) {
+			include($this->cachefile);
+			echo ("<!-- Cached ".date("jS F Y H:i", filemtime($this->cachefile))."-->");
+			exit;
+		}
+		ob_start();
 	}
 	
 	/**
 	 * Destruct method.
 	 */
-	function __destruct() {
-		
+	function endCache() {
+		$fp = fopen($this->cachefile, "w");
+		fwrite($fp, ob_get_contents());
+		fclose($fp);
+		ob_end_flush();
 	}
+	
 }
 ?>
