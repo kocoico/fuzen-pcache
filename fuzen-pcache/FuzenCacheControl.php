1068a19de9eedef4a36b02013290c47e9eb59ad1
Frederico Araujo Soares <frederico.a.soares@gmail.com>
2013-03-27 23:36:11 -0300

    Methods construct and destruct added.

--- fuzen-pcache/FuzenCacheControl.php
+++ fuzen-pcache/FuzenCacheControl.php
@@ -1,9 +1,26 @@
 <?php
 /**
- * Object to control cache pages.
- * @author Fred @web4u
+ * Php object to control cache pages.
+ * No framework and easy script.
+ * 
+ * @author Fred (twitter: @web4u)
+ * @license Free
+ * @version 0.1
  */
 class FuzenCacheControl {
 	
+	/**
+	 * Method construct.
+	 */
+	function __construct() {
+		
+	}
+	
+	/**
+	 * Destruct method.
+	 */
+	function __destruct() {
+		
+	}
 }
 ?>
